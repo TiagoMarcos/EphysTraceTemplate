7d946e589571598693622084f087219e1f9f2fa2
Tiago <tiago.marcos@gmx.co.uk>
2018-01-26 15:50:58 +0000

    Update rscript-tdeployment.R

--- rscript-tdeployment.R
+++ rscript-tdeployment.R
@@ -1,8 +1,16 @@
 #trace deployment script
 
 exp1 <-read.csv("file.csv",sep=",", header = TRUE, skip=1) #skip some rows if needed when importing file
+par(mfrow=c(2,3)) #multiple graphs in one
+#plot1
 plot(exp1[], exp1[], axes=FALSE, xlab = "", ylab = "", col="black",type="l") #strip axes
 segments(0.05, mean(exp1[1:1000,])+8, 0.15, mean(exp1[1:1000,])+8, col="black", lwd = 2) #scale bar
 segments(0.05, mean(exp1[1:1000,]+8), 0.05, mean(exp1[1:1000,])+18, col="black", lwd=2)
-
-
+#plot2
+plot(exp1[], exp1[], axes=FALSE, xlab = "", ylab = "", col="black",type="l") #strip axes
+segments(0.05, mean(exp1[1:1000,])+8, 0.15, mean(exp1[1:1000,])+8, col="black", lwd = 2) #scale bar
+segments(0.05, mean(exp1[1:1000,]+8), 0.05, mean(exp1[1:1000,])+18, col="black", lwd=2)
+#plot3
+plot(exp1[], exp1[], axes=FALSE, xlab = "", ylab = "", col="black",type="l") #strip axes
+segments(0.05, mean(exp1[1:1000,])+8, 0.15, mean(exp1[1:1000,])+8, col="black", lwd = 2) #scale bar
+segments(0.05, mean(exp1[1:1000,]+8), 0.05, mean(exp1[1:1000,])+18, col="black", lwd=2)
