077496c9d53c4bd48791c0be914b0e3875cbd01f
Tiago <tiago.marcos@gmx.co.uk>
2018-01-26 15:45:01 +0000

    Update rscript-tdeployment.R

--- rscript-tdeployment.R
+++ rscript-tdeployment.R
@@ -2,7 +2,7 @@
 
 exp1 <-read.csv("file.csv",sep=",", header = TRUE, skip=1) #skip some rows if needed when importing file
 plot(exp1[], exp1[], axes=FALSE, xlab = "", ylab = "", col="black",type="l") #strip axes
-segments(0.05, mean(exp1[])+8, 0.15, mean(exp1[])+8, col="black", lwd = 2) #scale bar
-segments(0.05, mean(exp1[]+8), 0.05, mean(exp1[])+18, col="black", lwd=2)
+segments(0.05, mean(exp1[1:1000,])+8, 0.15, mean(exp1[1:1000,])+8, col="black", lwd = 2) #scale bar
+segments(0.05, mean(exp1[1:1000,]+8), 0.05, mean(exp1[1:1000,])+18, col="black", lwd=2)
 
 
