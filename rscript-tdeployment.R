cb351f6ebaa056b91614fe817f97ba81f7b93aa9
Tiago <tiago.marcos@gmx.co.uk>
2018-01-26 12:39:33 +0000

    Update rscript-tdeployment.R

--- rscript-tdeployment.R
+++ rscript-tdeployment.R
@@ -5,3 +5,4 @@ plot(exp1[], exp1[], axes=FALSE, xlab = "", ylab = "" col="black",type="l") #str
 segments(0.05, mean(exp1[])+8, 0.15, mean(exp1[])+8, col="black", lwd = 2) #scale bar
 segments(0.05, mean(exp1[]+8), 0.05, mean(exp1[])+18, col="black", lwd=2)
 
+
